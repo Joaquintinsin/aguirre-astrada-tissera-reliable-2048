cc8e8d51db0ac4f3f0f68831b36e463711b2fa67
Joaquin Tissera <phoenix_css@hotmail.com>
2026-08-13 15:29:19 -0300

    Add getEmptyPosition Test

--- src/test/java/ar/edu/unrc/game2048/BoardTest.java
+++ src/test/java/ar/edu/unrc/game2048/BoardTest.java
@@ -1,7 +1,34 @@
 package ar.edu.unrc.game2048;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
-class BoardTest {
+import ar.edu.unrc.game2048.Board.Position;
 
+class BoardTest {
+    @Test
+    public void getEmptyPositionTest() {
+        // Arrange
+        int size = 2;
+        Board boardToTest = new Board(size);
+
+        // Act
+        Set<Position> emptyPositions = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Cell cellToTest = boardToTest.getCell(i, j);
+                if (cellToTest.isEmpty()) {
+                    Position cellPosition = new Position(i, j);
+                    emptyPositions.add(cellPosition);
+                }
+            }
+        }
+
+        // Assert
+        assert boardToTest.getEmptyPositions().equals(emptyPositions);
+    }
 }
