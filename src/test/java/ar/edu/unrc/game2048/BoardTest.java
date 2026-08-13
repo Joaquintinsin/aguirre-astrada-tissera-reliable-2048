625c59082255e475464a5feecca6b353eb0f5b3b
camiastrada <camiastradaw@gmail.com>
2026-08-13 15:04:21 -0300

    Add default constructor Test

--- src/test/java/ar/edu/unrc/game2048/BoardTest.java
+++ src/test/java/ar/edu/unrc/game2048/BoardTest.java
@@ -1,7 +1,16 @@
 package ar.edu.unrc.game2048;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
+    @Test
+    public void defaultConstructorTest() {
+        // Arrange - Act
+        Board board = new Board();
+        // Assert
+        assertNotNull(board);
+    }
 
 }
