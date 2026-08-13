f19c33da44cd96edb146f11eae2ff6f3474158b8
camiastrada <camiastradaw@gmail.com>
2026-08-13 15:15:41 -0300

    add test for parameter constructor negative size exception

--- src/test/java/ar/edu/unrc/game2048/BoardTest.java
+++ src/test/java/ar/edu/unrc/game2048/BoardTest.java
@@ -2,6 +2,7 @@ package ar.edu.unrc.game2048;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,14 +15,24 @@ class BoardTest {
         assertNotNull(board);
     }
 
-    @Test 
+    @Test
     public void parameterConstructorTest() {
         int inputSize = 6;
         // Arrange - Act
-        Board board = new Board(6);
+        Board board = new Board(inputSize);
         // Assert
         assertEquals(board.getSize(), inputSize);
-        
+
     }
 
+    @Test 
+    public void parameterConstructorNegativeNumberExeptionTest() {
+        int inputSize = -1;
+        // Arrange - Act
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+          Board board = new Board(inputSize);
+        });
+        
+    }
 }
