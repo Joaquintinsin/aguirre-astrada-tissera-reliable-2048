67eb6541ce0d26c026a49afd96c10fdb0eb5edf0
TrinidadSA <aguirretrini97@gmail.com>
2026-08-16 22:43:25 -0300

    Add unit tests to Cell - equals, hashCode and toString

--- src/test/java/ar/edu/unrc/game2048/CellTest.java
+++ src/test/java/ar/edu/unrc/game2048/CellTest.java
@@ -12,7 +12,6 @@ class CellTest {
     void testCellCreation() {
         //arrange-act
         Cell cell = new Cell(2);
-    
         //assert
         assertEquals(cell.getValue(), 2);
     }
@@ -21,10 +20,8 @@ class CellTest {
     void testIsEmptyTrue() {
         //arrange
         Cell cell = new Cell(0);
-
         //act
         boolean result = cell.isEmpty();
-
         //assert
         assertTrue(result);
     }
@@ -33,10 +30,8 @@ class CellTest {
     void testIsEmptyFalse() {
         //arrange
         Cell cell = new Cell(4);
-
         //act
         boolean result = cell.isEmpty();
-
         //assert
         assertFalse(result);
     }
@@ -45,10 +40,8 @@ class CellTest {
     void testGetValueEmpty() {
         //arrange
         Cell cell = new Cell(0);
-
         //act
         int value = cell.getValue();
-
         //assert
         assertEquals(value, 0);
     }
@@ -57,10 +50,8 @@ class CellTest {
     void testGetValueNonEmpty() {
         //arrange
         Cell cell = new Cell(8);
-
         //act
         int value = cell.getValue();
-
         //assert
         assertEquals(value, 8);
     }
@@ -70,10 +61,8 @@ class CellTest {
         //arrange
         Cell cell1 = new Cell(4);
         Cell cell2 = new Cell(4);
-
         //act
         boolean result = cell1.canMergeWith(cell2);
-
         //assert
         assertTrue(result);
     }
@@ -83,10 +72,8 @@ class CellTest {
         //arrange
         Cell cell1 = new Cell(2);
         Cell cell2 = new Cell(4);
-
         //act
         boolean result = cell1.canMergeWith(cell2);
-
         //assert
         assertFalse(result);
     }
@@ -96,10 +83,8 @@ class CellTest {
         //arrange
         Cell cell1 = new Cell(0);
         Cell cell2 = new Cell(0);
-
         //act
         boolean result = cell1.canMergeWith(cell2);
-
         //assert
         assertFalse(result);
     }
@@ -109,10 +94,8 @@ class CellTest {
         //arrange
         Cell cell1 = new Cell(4);
         Cell cell2 = new Cell(4);
-
         //act
         Cell mergedCell = cell1.mergeWith(cell2);
-
         //assert
         assertEquals(mergedCell.getValue(), 8);
     }
@@ -122,9 +105,63 @@ class CellTest {
         //arrange
         Cell cell1 = new Cell(2);
         Cell cell2 = new Cell(4);
-
         //act - assert
         assertThrows(IllegalArgumentException.class, () -> cell1.mergeWith(cell2));
+    }
+
+
+    @Test
+    void testEqualsTrue() {
+        //arrange
+        Cell cell1 = new Cell(16);
+        Cell cell2 = new Cell(16);
+        //act
+        boolean result = cell1.equals(cell2);
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void testEqualsFalse() {
+        //arrange
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(4);
+        //act
+        boolean result = cell1.equals(cell2);
+        //assert
+        assertFalse(result);
+    }
+
+    @Test
+    void testHashCode() {
+        //arrange
+        Cell cell1 = new Cell(32);
+        Cell cell2 = new Cell(32);
+        //act
+        int hash1 = cell1.hashCode();
+        int hash2 = cell2.hashCode();
+        //assert
+        assertEquals(hash1, hash2);
+    }
+
+    @Test
+    void testToStringValue() {
+        //arrange
+        Cell cell = new Cell(64);
+        //act
+        String str = cell.toString();
+        //assert
+        assertEquals(str, "64");
+    }
+
+    @Test
+    void testToStringEmpty() {
+        //arrange
+        Cell cell = new Cell(0);
+        //act
+        String str = cell.toString();
+        //assert
+        assertEquals(str, ".");
     }
 
 
