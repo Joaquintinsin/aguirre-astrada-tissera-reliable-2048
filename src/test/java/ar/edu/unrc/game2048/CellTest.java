ffc116dcf881c5b256ba0bc1b3611f5e6218b2af
TrinidadSA <aguirretrini97@gmail.com>
2026-08-16 22:29:41 -0300

    Add unit tests to Cell, getValue and check merge

--- src/test/java/ar/edu/unrc/game2048/CellTest.java
+++ src/test/java/ar/edu/unrc/game2048/CellTest.java
@@ -2,6 +2,7 @@ package ar.edu.unrc.game2048;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -39,6 +40,95 @@ class CellTest {
         //assert
         assertFalse(result);
     }
+
+    @Test
+    void testGetValueEmpty() {
+        //arrange
+        Cell cell = new Cell(0);
+
+        //act
+        int value = cell.getValue();
+
+        //assert
+        assertEquals(value, 0);
+    }
+
+    @Test
+    void testGetValueNonEmpty() {
+        //arrange
+        Cell cell = new Cell(8);
+
+        //act
+        int value = cell.getValue();
+
+        //assert
+        assertEquals(value, 8);
+    }
+
+    @Test
+    void testCanMergeWithTrue() {
+        //arrange
+        Cell cell1 = new Cell(4);
+        Cell cell2 = new Cell(4);
+
+        //act
+        boolean result = cell1.canMergeWith(cell2);
+
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void testCanMergeWithFalse() {
+        //arrange
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(4);
+
+        //act
+        boolean result = cell1.canMergeWith(cell2);
+
+        //assert
+        assertFalse(result);
+    }
+
+    @Test
+    void testCanMergeWithEmptyCells() {
+        //arrange
+        Cell cell1 = new Cell(0);
+        Cell cell2 = new Cell(0);
+
+        //act
+        boolean result = cell1.canMergeWith(cell2);
+
+        //assert
+        assertFalse(result);
+    }
+
+    @Test
+    void testMergeSussessful() {
+        //arrange
+        Cell cell1 = new Cell(4);
+        Cell cell2 = new Cell(4);
+
+        //act
+        Cell mergedCell = cell1.mergeWith(cell2);
+
+        //assert
+        assertEquals(mergedCell.getValue(), 8);
+    }
+
+    @Test 
+    void testMergeFailWithException() {
+        //arrange
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(4);
+
+        //act - assert
+        assertThrows(IllegalArgumentException.class, () -> cell1.mergeWith(cell2));
+    }
+
+
+
 
     
 }
