f92692b6d2537eb85e53e28210ac4fe464c935a3
TrinidadSA <aguirretrini97@gmail.com>
2026-08-16 21:59:33 -0300

    Add unit tests to isEmpty method

--- src/test/java/ar/edu/unrc/game2048/CellTest.java
+++ src/test/java/ar/edu/unrc/game2048/CellTest.java
@@ -1,6 +1,8 @@
 package ar.edu.unrc.game2048;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class CellTest {
@@ -12,6 +14,30 @@ class CellTest {
     
         //assert
         assertEquals(cell.getValue(), 2);
+    }
+
+    @Test
+    void testIsEmptyTrue() {
+        //arrange
+        Cell cell = new Cell(0);
+
+        //act
+        boolean result = cell.isEmpty();
+
+        //assert
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsEmptyFalse() {
+        //arrange
+        Cell cell = new Cell(4);
+
+        //act
+        boolean result = cell.isEmpty();
+
+        //assert
+        assertFalse(result);
     }
 
     
