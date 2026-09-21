862697e64ec7af9e4cc9b97c0e99e09efa64634c
camiastrada <camiastradaw@gmail.com>
2026-09-21 18:56:34 -0300

    modify pom and Evosuite tests for generating JaCoCo test report

--- 5-testearEvosuite.sh
+++ 5-testearEvosuite.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Para correr este script se necesita que java sea 1.8
+# Corre solo los tests generados por EvoSuite (*_ESTest) y genera el reporte de JaCoCo
+# Requiere separateClassLoader = false en los *_ESTest.java
+# Reporte: target/site/jacoco/index.html
 
-
-# Run tests
-mvn -f pom-evosuite.xml clean compile
-mvn -f pom-evosuite.xml test
+mvn -f pom-evosuite.xml clean test jacoco:report -Dtest='*_ESTest'
--- src/test/java/EvosuiteTests/NonDeterministicPlacement_ESTest.java
+++ src/test/java/EvosuiteTests/NonDeterministicPlacement_ESTest.java
@@ -15,7 +15,7 @@ import org.evosuite.runtime.EvoRunnerParameters;
 import org.evosuite.runtime.Random;
 import org.junit.runner.RunWith;
 
-@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true, useJEE = true) 
+@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = false, useJEE = true) 
 public class NonDeterministicPlacement_ESTest extends NonDeterministicPlacement_ESTest_scaffolding {
 
   @Test(timeout = 4000)
